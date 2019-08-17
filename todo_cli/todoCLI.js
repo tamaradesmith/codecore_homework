09136f43c1ec14aef4e92e0b46649704cec72418
Tamara Smith <tdesmith.shaw.ca>
2019-08-17 14:26:32 -0700

    updated todoCLI with remain commands

--- todo_cli/todoCLI.js
+++ todo_cli/todoCLI.js
@@ -1,69 +1,83 @@
-// const fs = reqiure('fs');
 const readline = require('readline');
 const fs = require('fs');
-// const fs = require('fs');
-
 
 const interface = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 })
 
-// const interface = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout,
-// });
 
 // The Menu
-// When the todoCLI.js is first executed, a menu as shown below should be displayed. These are all the options the user should be able to perform.
 
-// $ node todoCLI.js
+let fileName;
+let nextNumber;
 
-// Welcome to Todo CLI!
-// --------------------
-// (v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit
-// >
-
-
-
-const welcome = "Welcome to Todo CLI! \n--------------------\n(v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit \n"
-
-function menu() {
-    let nextNumber;
-    fs.readFile('task_list.txt', (err, data) => {
+if (process.argv[2] === undefined) {
+    fileName = 'task_list.txt';
+    fs.readFile(fileName, (err, data) => {
         if (err) {
             console.log(error);
         } else {
             let temp = data.toString().split('\n');
-           return nextNumber = parseInt( temp[temp.length-1][0]) + 1;            
+            return nextNumber = parseInt(temp[temp.length - 1][0]) + 1;
         }
     })
+} else {
+    fileName = process.argv[2];
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            fs.writeFile(fileName, "List is empty", (err) => {
+                if (err) {
+                    console.log("error in creating list file")
+
+                } else {
+
+                    nextNumber = 0;
+                }
+            })
+        } else {
+            fs.readFile(fileName, (err, data) => {
+                if (err) {
+                    console.log(error);
+                } else {
+                    let temp = data.toString().split('\n');
+                    return nextNumber = parseInt(temp[temp.length - 1][0]) + 1;
+                }
+            })
+        }
+    })
+}
+console.log(nextNumber)
+const welcome = "Welcome to Todo CLI! \n--------------------\n(v) View • (n) Nw • (cX) Complete • (dX) Delete • (q) Quit \n"
+
+function menu() {
+
     interface.question(welcome, (answer) => {
         let num;
-        if (answer.includes("c")){
+        if (answer.includes("c") || answer.includes("d")) {
             num = answer.slice(1);
         }
         switch (answer) {
             case "v":
                 return viewTask();
-            break;
+                break;
             case "n":
                 return addTask(nextNumber);
                 break;
             case `c${num}`:
                 return completeTask(num);
                 break;
-            case "dX":
-                console.log("Delete Task");
+            case `d${num}`:
+                return deleteTask(num);
                 break;
             case "q":
-                console.log("Quit");
+                console.log("Farewell have a lovey day, don't forget to smile!!");
                 process.exit();
                 break;
             default:
-                console.log("Invaled anwser")
+                console.log("Invaled anwser, Please try again")
+                menu()
         }
-        // console.log("finish")
         process.exit();
     })
 }
@@ -71,15 +85,14 @@ function menu() {
 
 // View
 
-
 function viewTask() {
-    fs.readFile('task_list.txt', (err, data) => {
+    fs.readFile(fileName, (err, data) => {
         if (err) {
             console.log("error reading file");
         } else {
             let task = data.toString().split("\n");
-            console.log(`${task.join('\n')}\n`)
-            menu()
+            console.log(`${task.join('\n')}\n`);
+            menu();
         }
     })
 
@@ -88,64 +101,90 @@ function viewTask() {
 // Add
 
 function addTask(num) {
-    interface.question('What is the new task?', (anwser) => {
+    interface.question('What is the new task?  ', (anwser) => {
         let task = `\n${num} [ ] ${anwser}`
-        fs.appendFile('task_list.txt', task, (err) => {
-            if (err) {
-                console.log('problem adding new task')
-            } else {
-                console.log("new Task added")
-                menu();
-                return num++
-            }
-        })
+        if (num !== 0) {
+            fs.appendFile(fileName, task, (err) => {
+                if (err) {
+                    console.log('problem adding new task');
+                } else {
+                    console.log(`Added task: ${anwser}\n`);
+                    menu();
+                    return num++;
+                }
+            })
+        } else {
+            fs.writeFile(fileName, task, (err)=>{
+                if (err){
+                    console.log("Problem adding new task");
+                } else {
+                    console.log(`Added task: ${anwser}\n`);
+                    menu();
+                    return num++;
+                }
+            })
+        }
 
-    })
+})
 }
 
-
 // Complete
-// From the Todo Menu pressing cX where X refers to the index of a Todo item then Enter should mark that item as complete. Tell the user which item was marked. Then, re-display the Todo Menu.
 
-// $ node todoCLI.js
-
-// Welcome to Todo CLI!
-// --------------------
-// (v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit
-// > c2
-
-// Completed "Buy Snickerdoodles"
-
-// (v) View • (n) New • (cX) Complete • (dX) Delete • (q) Quit
-// >
-// When displaying the list, completed items should have a checkmark (i.e. ✓) besides their title. For example:
-
-// 0 [✓] Take out the trash
-
-// function completeTask (){
-//     fs.readFile()
-// }
-function completeTask(n){
-    fs.readFile('task_list.txt', (err, data) =>{
-        if(err){
+function completeTask(n) {
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
             console.log(" Error reading file")
         } else {
-           let comTask =data.toString().split('\n')[n];
-           let task = comTask.slice(0,3) + "✓" + comTask.slice(4)
-           console.log(task)
-        //    need to replace Task in file
-           menu();
+            let list = data.toString().split('\n')
+            let comTask = list[n];
+            let task = comTask.slice(0, 3) + "✓" + comTask.slice(4)
+            list.splice(n, 1, task)
+            list = list.join('\n');
+            fs.writeFile(fileName, list, (err) => {
+                if (err) {
+                    console.log('error writing file')
+                } else {
+                    console.log(`Task ${comTask} completed \n`)
+                    menu();
+                }
+            })
         }
     })
 };
 
+// Delete
 
-
-
-
-
-
-
+function deleteTask(n) {
+    console.log("delete");
+    fs.readFile(fileName, (err, data) => {
+        if (err) {
+            console.log("error with reading file")
+        } else {
+            let list = data.toString().split('\n');
+            let result = [];
+            let index = 0;
+            let deletedTask;
+            for (let i = 0; i < list.length; i++) {
+                if (i !== parseInt(n)) {
+                    result.push(`${index}${list[i].slice(1)}`)
+                    index++;
+                } else {
+                    deletedTask = `${list[i].slice(5)}`;
+                }
+            }
+            nextNumber = index++
+            list = result.join('\n');
+            fs.writeFile(fileName, list, (err) => {
+                if (err) {
+                    console.log('error deleting task');
+                } else {
+                    console.log(`Deleted: ${deletedTask} /n`);
+                    menu();
+                }
+            })
+        }
+    })
+}
 
 
 menu();
