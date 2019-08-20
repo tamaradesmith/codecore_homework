6440f4d6cf1b5dac82881d39b2fb0a383fb9c0d1
Tamara Smith <tdesmith.shaw.ca>
2019-08-20 16:43:19 -0700

    add save option and fix bugs

--- todo_cli/todoCLI.js
+++ todo_cli/todoCLI.js
@@ -18,6 +18,7 @@ if (process.argv[2] === undefined) {
             console.log(error);
         } else {
             let temp = data.toString().split('\n');
+            fileRead()
             return nextNumber = parseInt(temp[temp.length - 1][0]) + 1;
         }
     })
@@ -30,16 +31,17 @@ if (process.argv[2] === undefined) {
                     console.log("error in creating list file")
 
                 } else {
-
+                    taskList = "List is empty";
                     nextNumber = 0;
                 }
             })
         } else {
             fs.readFile(fileName, (err, data) => {
                 if (err) {
-                    console.log(error);
+                    console.log("error reading file");
                 } else {
                     let temp = data.toString().split('\n');
+                    fileRead()
                     return nextNumber = parseInt(temp[temp.length - 1][0]) + 1;
                 }
             })
@@ -47,20 +49,19 @@ if (process.argv[2] === undefined) {
     })
 }
 console.log(nextNumber)
-const welcome = "Welcome to Todo CLI! \n--------------------\n(v) View • (n) Nw • (cX) Complete • (dX) Delete • (q) Quit \n"
+const welcome = "Welcome to Todo CLI! \n--------------------\n(v) View • (n) New • (cX) Complete • (dX) Delete • (s) Save • (q) Quit \n"
 
 function menu() {
-
     interface.question(welcome, (answer) => {
         let num;
         if (answer.includes("c") || answer.includes("d")) {
-            num = answer.slice(1);
+            answer.length < 2 ? answer = '' : num = answer.slice(1);
         }
-        switch (answer) {
-            case "v":
+        switch (answer.toLowerCase()) {
+            case `v`:
                 return viewTask();
                 break;
-            case "n":
+            case `n`:
                 return addTask(nextNumber);
                 break;
             case `c${num}`:
@@ -69,15 +70,17 @@ function menu() {
             case `d${num}`:
                 return deleteTask(num);
                 break;
-            case "q":
+            case `q`:
                 console.log("Farewell have a lovey day, don't forget to smile!!");
                 process.exit();
+                break;
+            case `s`:
+                return saveList();
                 break;
             default:
                 console.log("Invaled anwser, Please try again")
                 menu()
         }
-        process.exit();
     })
 }
 
@@ -93,21 +96,24 @@ function viewTask() {
 
 function addTask(num) {
     interface.question('What is the new task?  ', (anwser) => {
-        let list = `\n${num} [ ] ${anwser}`
         let message = `Added task: ${anwser}`
+        let list = `\n${num} [ ] ${anwser}`
         if (num !== 0) {
             fs.appendFile(fileName, list, (err) => {
                 if (err) {
                     console.log('problem adding new task');
                 } else {
                     console.log(`Added task: ${anwser}\n`);
+                    nextNumber++;
+                    taskList += list;
                     menu();
-                    return num++;
                 }
             })
         } else {
-            fileName(list, message)
-            return num++;
+            let list = `${num} [ ] ${anwser}`
+            fileWrite(list, message)
+            taskList += list;
+            return nextNumber++;
         }
     })
 }
@@ -116,6 +122,11 @@ function addTask(num) {
 
 function completeTask(n) {
     let list = taskList.split('\n')
+    if (n > list.length) {
+        console.log(`Task doesn't exist`);
+        menu();
+        return;
+    }
     let comTask = list[n];
     let task = comTask.slice(0, 3) + "✓" + comTask.slice(4)
     list.splice(n, 1, task)
@@ -145,15 +156,35 @@ function deleteTask(n) {
     fileWrite(list, message)
 }
 
+//  Save to new file:
+
+function saveList() {
+    interface.question("What is the new file name and type? ", (anwser) => {
+        fs.writeFile(anwser, taskList, (err) => {
+            if (err) {
+                console.log('Error with file')
+            } else {
+                fileName = anwser;
+                menu();
+            }
+        });
+    })
+}
+
+
+// Read task file 
+
 function fileRead() {
     fs.readFile(fileName, (err, data) => {
         if (err) {
-            console.log("error with reading file");
+            console.log(" read error with reading file");
         } else {
             taskList = data.toString();
         }
     })
 }
+
+// write task file:
 
 function fileWrite(list, message) {
     fs.writeFile(fileName, list, (err) => {
@@ -161,7 +192,6 @@ function fileWrite(list, message) {
             console.log('Error writing file');
         } else {
             console.log(`${message} \n`);
-
             taskList = list;
             menu();
         }
@@ -169,5 +199,4 @@ function fileWrite(list, message) {
 }
 
 
-fileRead();
 menu();
